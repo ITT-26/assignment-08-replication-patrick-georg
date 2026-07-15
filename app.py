53a3ebda3d1e84982276aba3c9854bf2d5857f58
Georg Dechant <georg_dechant@web.de>
2026-07-15 20:09:31 +0200

    main app
    starts main window after setup
    
    Co-Authored-By: Patrick Balla <63912835+rawrick@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,27 @@
+import pyglet
+from setup_window import SetupWindow
+from main_window import MainWindow
+
+
+# launch app method
+def launch_app():
+
+    # state for main window
+    state = {"main_window": None}
+
+    # callback for when setup is confirmed
+    def on_setup_confirm(device_index, handedness):
+
+        # MainWindow with selected options
+        state["main_window"] = MainWindow(
+            device_index=device_index,
+            handedness=handedness,
+        )
+
+    # start setup window with callback
+    SetupWindow(on_setup_confirm)
+    pyglet.app.run()
+
+
+if __name__ == "__main__":
+    launch_app()
